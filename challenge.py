eaf12d6cd695e07d4b8786a76e88d2a94ae3f7c2
Cristhian Castillo <cristhianeduardo03@hotmail.com>
2020-05-21 16:57:33 -0500

    Added tests and setup

--- challenge.py
+++ challenge.py
@@ -57,32 +57,49 @@ if __name__ == '__main__':
     class GeometrySuite(unittest.TestCase):
 
         def setUp(self):
-            # Initialize the needed values for the tests
-            pass
+            self.values = {
+                'side': 0,
+                'base': 0,
+                'height': 0,
+                'diagonal_1': 0,
+                'diagonal_2': 0,
+                'base_minor': 0,
+                'base_major': 0,
+                'perimeter': 0,
+                'apothem': 0,
+                'radius': 0
+            }
 
         def test_square_area(self):
-            # Make this test first...
+            """Test the function to find the area of a square with a side greater than zero"""
+            self.assertEqual(0, square_area(self.values['side']))
 
         def test_rectangle_area(self):
-            # Make this test first...
+            """Test the function to find the area of a rectangle with height and base greater than zero"""
+            self.assertEqual(0, rectangle_area(self.values['base'], self.values['height']))
 
         def test_triangle_area(self):
-            # Make this test first...
+            """Test the function to find the area of a triangle with height and base greater than zero"""
+            self.assertEqual(0, triangle_area(self.values['base'], self.values['height']))
 
         def test_rhombus_area(self):
-            # Make this test first...
+            """Test the function to find the area of a rhombus with diagonals greater than zero"""
+            self.assertEqual(0, rhombus_area(self.values['diagonal_1'], self.values['diagonal_2']))
 
         def test_trapezoid_area(self):
-            # Make this test first...
+            """Test the function to find the area of a rhombus with bases greater than zero and height greater than zero"""
+            self.assertEqual(0, trapezoid_area(self.values['base_minor'], self.values['base_major'], self.values['height']))
 
         def test_regular_polygon_area(self):
-            # Make this test first...
+            """Test the function to find the area of a polygon with apothem and perimeter greater than zero"""
+            self.assertEqual(0, regular_polygon_area(self.values['perimeter'], self.values['apothem']))
+
 
         def test_circumference_area(self):
-            # Make this test first...
+            """Test the function to find the area of a circle with radius greater than zero"""
+            self.assertEqual(0, circumference_area(self.values['radius']))
 
         def tearDown(self):
-            # Delete the needed values for the tests
-            pass
+            del(self.values)
 
     unittest.main()
