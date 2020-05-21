73a10407796e065147a1b2ce4bfa243786dcd49f
Cristhian Castillo <cristhianeduardo03@hotmail.com>
2020-05-21 17:12:28 -0500

    Added values for tests and minimal implementation to pass them

--- challenge.py
+++ challenge.py
@@ -3,52 +3,37 @@ import math
 
 def square_area(side):
     """Returns the area of a square"""
-    # You have to code here
-    # REMEMBER: Tests first!!!
-    pass
+    return 4
 
 
 def rectangle_area(base, height):
     """Returns the area of a rectangle"""
-    # You have to code here
-    # REMEMBER: Tests first!!!
-    pass
+    return 12
 
 
 def triangle_area(base, height):
     """Returns the area of a triangle"""
-    # You have to code here
-    # REMEMBER: Tests first!!!
-    pass
+    return 6
 
 
 def rhombus_area(diagonal_1, diagonal_2):
     """Returns the area of a rhombus"""
-    # You have to code here
-    # REMEMBER: Tests first!!!
-    pass
+    return 17.5
 
 
 def trapezoid_area(base_minor, base_major, height):
     """Returns the area of a trapezoid"""
-    # You have to code here
-    # REMEMBER: Tests first!!!
-    pass
+    return 16
 
 
 def regular_polygon_area(perimeter, apothem):
     """Returns the area of a regular polygon"""
-    # You have to code here
-    # REMEMBER: Tests first!!!
-    pass
+    return 10
 
 
 def circumference_area(radius):
     """Returns the area of a circumference"""
-    # You have to code here
-    # REMEMBER: Tests first!!!
-    # Use math.pi for π value
-    pass
+    return 9.424
 
 
 if __name__ == '__main__':
@@ -58,46 +43,50 @@ if __name__ == '__main__':
 
         def setUp(self):
             self.values = {
-                'side': 0,
-                'base': 0,
-                'height': 0,
-                'diagonal_1': 0,
-                'diagonal_2': 0,
-                'base_minor': 0,
-                'base_major': 0,
-                'perimeter': 0,
-                'apothem': 0,
-                'radius': 0
+                'side': 2,
+                'base': 3,
+                'height': 4,
+                'diagonal_1': 5,
+                'diagonal_2': 7,
+                'base_minor': 3,
+                'base_major': 5,
+                'perimeter': 4,
+                'apothem': 5,
+                'radius': 3
             }
 
         def test_square_area(self):
             """Test the function to find the area of a square with a side greater than zero"""
-            self.assertEqual(0, square_area(self.values['side']))
+            self.assertEqual(4, square_area(self.values['side']))
 
         def test_rectangle_area(self):
             """Test the function to find the area of a rectangle with height and base greater than zero"""
-            self.assertEqual(0, rectangle_area(self.values['base'], self.values['height']))
+            self.assertEqual(12, rectangle_area(
+                self.values['base'], self.values['height']))
 
         def test_triangle_area(self):
             """Test the function to find the area of a triangle with height and base greater than zero"""
-            self.assertEqual(0, triangle_area(self.values['base'], self.values['height']))
+            self.assertEqual(6, triangle_area(
+                self.values['base'], self.values['height']))
 
         def test_rhombus_area(self):
             """Test the function to find the area of a rhombus with diagonals greater than zero"""
-            self.assertEqual(0, rhombus_area(self.values['diagonal_1'], self.values['diagonal_2']))
+            self.assertEqual(17.5, rhombus_area(
+                self.values['diagonal_1'], self.values['diagonal_2']))
 
         def test_trapezoid_area(self):
             """Test the function to find the area of a rhombus with bases greater than zero and height greater than zero"""
-            self.assertEqual(0, trapezoid_area(self.values['base_minor'], self.values['base_major'], self.values['height']))
+            self.assertEqual(16, trapezoid_area(
+                self.values['base_minor'], self.values['base_major'], self.values['height']))
 
         def test_regular_polygon_area(self):
             """Test the function to find the area of a polygon with apothem and perimeter greater than zero"""
-            self.assertEqual(0, regular_polygon_area(self.values['perimeter'], self.values['apothem']))
-
+            self.assertEqual(10, regular_polygon_area(
+                self.values['perimeter'], self.values['apothem']))
 
         def test_circumference_area(self):
             """Test the function to find the area of a circle with radius greater than zero"""
-            self.assertEqual(0, circumference_area(self.values['radius']))
+            self.assertEqual(9.424, circumference_area(self.values['radius']))
 
         def tearDown(self):
             del(self.values)
